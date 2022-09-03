43529e65c2ec6eaf1972f0d68a928949f8ece3da
“Housam <housam.kak20@gmail.com>
2022-09-03 22:55:22 +0300

    Added Your Score block

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ function()
     starting_point=document.getElementById("start");
     starting_point.onclick=function(){reset()}
 
+    
     // The borders turn red if they are touched, if we touch any element they will become red,i.e. we loop over all them, because 1 is enough
     //  to achieve the condition.
     borders=document.getElementsByClassName("boundary");
@@ -13,10 +14,36 @@ function()
     {
     borders[i].onmouseover = function(){mouseOver()};
     }
+    // Score Count
+    x=0;
+    lis=document.createElement("ul");
+    para1=document.createElement("p");
+    para2=document.createElement("p");
+    borders[2].appendChild(lis);
+    lis.appendChild(para2)
+    lis.insertBefore(para1,para2)
+    lis.style.display="flex";
+    lis.style.flexDirection="column";
+    lis.style.fontSize="xx-large";
+    lis.style.marginLeft="12rem";
+    para1.style.margimBottom="0px";
+    para2.style.marginTop="0rem";
+    para2.style.marginLeft="4rem";
+    para1.appendChild(document.createTextNode("Your score:"));
+    para2.appendChild(document.createTextNode(x));
+    console.log(borders[2])
+    
+    // <div class="boundary">
+	// 			<ul style="display:flex;flex-direction: column;font-size:xx-large;margin-left: 12rem;">
+	// 				<p style="margin-bottom:0">Your score:</p>
+	// 				<p style="margin-top:0;margin-left: 4rem;">7</p>
+	// 			</ul>
+	// 		</div>
+    
 
     // Wining condition
     ending_point=document.getElementById("end");
-    ending_point.onmouseover = function(){document.getElementById("status").textContent='You Won :)'};
+    ending_point.onmouseover = function(){document.getElementById("status").textContent='You Won :)';x+=5};
 
     // Add restart button
     // borders[5].textContent='Restart';
