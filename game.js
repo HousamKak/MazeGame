7b4ad0cc51c3e9a758e37b6763151ea1d90610b9
“Housam <housam.kak20@gmail.com>
2022-09-03 21:24:11 +0300

    Added loss message and reset to original message

--- game.js
+++ game.js
@@ -17,23 +17,33 @@ function()
     // Functions Section
 
     // If the the elements were hovered over all the list becomes red
+    // A lost message appears
     function mouseOver()
     {
+        // becoming red
         borders=document.getElementsByClassName("boundary");
         for(i=0;i<borders.length-1;i++)
         {
         borders[i].style.backgroundColor="red";
         }
+
+        // loss message
+        document.getElementById("status").textContent='You lost :(';
+        
     }
 
     // If the start button is pressed the boundaries return to their original color
     function reset()
     {
+        // reseting color
         orders=document.getElementsByClassName("boundary");
         for(i=0;i<borders.length-1;i++)
         {
         borders[i].style.backgroundColor="#eeeeee";
         }
+    
+        // reseting message
+        document.getElementById("status").textContent='Begin by moving your mouse over the "S"';
     }
 }
 )
