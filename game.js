c6ea0e7bfdce40a4b6ffe452709f6b891b5a5928
“Housam <housam.kak20@gmail.com>
2022-09-03 21:14:43 +0300

    Added Click Event: The boundaries returns to original color when S block is clicked Added Comments

--- game.js
+++ game.js
@@ -2,19 +2,37 @@ $(document).ready
 (
 function()
 {
-    
+    // The game resets upon pressing the starting point S
+    starting_point=document.getElementById("start");
+    starting_point.onclick=function(){reset()}
+
+    // The borders turn red if they are touched, if we touch any element they will become red,i.e. we loop over all them, because 1 is enough
+    //  to achieve the condition.
     borders=document.getElementsByClassName("boundary");
     for(i=0;i<borders.length-1;i++)
     {
     borders[i].onmouseover = function(){mouseOver()};
     }
 
+    // Functions Section
+
+    // If the the elements were hovered over all the list becomes red
     function mouseOver()
     {
         borders=document.getElementsByClassName("boundary");
         for(i=0;i<borders.length-1;i++)
         {
         borders[i].style.backgroundColor="red";
+        }
+    }
+
+    // If the start button is pressed the boundaries return to their original color
+    function reset()
+    {
+        orders=document.getElementsByClassName("boundary");
+        for(i=0;i<borders.length-1;i++)
+        {
+        borders[i].style.backgroundColor="#eeeeee";
         }
     }
 }
