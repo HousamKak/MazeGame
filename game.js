f94477817377ed5be892f8e953c6306c6969856a
“Housam <housam.kak20@gmail.com>
2022-09-03 21:04:14 +0300

    Changed Color of the whole boundary upon hovering

--- game.js
+++ game.js
@@ -4,25 +4,18 @@ function()
 {
     
     borders=document.getElementsByClassName("boundary");
-    // borders[0].style.backgroundColor="red"
-    console.log(borders.length)
-    borders[0].onmouseover = function(){mouseOver(0)};
-    // for(i=0;i<borders.length;i++)
-    // {
-    // borders[i].onmouseover = function(){mouseOver(i)};
-    // borders[i].onmouseout = function() {mouseOut(i)};
-    // }
-
-    function mouseOver(i)
+    for(i=0;i<borders.length;i++)
     {
-        borders=document.getElementsByClassName("boundary");
-        borders[i].style.backgroundColor="red";
-
+    borders[i].onmouseover = function(){mouseOver()};
     }
-    function mouseOut(i)
+
+    function mouseOver()
     {
         borders=document.getElementsByClassName("boundary");
-        borders[i].style.backgroundColor="#eeeeee";
+        for(i=0;i<borders.length;i++)
+        {
+        borders[i].style.backgroundColor="red";
+        }
     }
 }
 )
